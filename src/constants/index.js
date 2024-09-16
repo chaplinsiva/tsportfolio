59d34efef5ded2e09f7b04a8907678609ef1aec6
chaplinsiva <talkschaplin@gmail.com>
2024-09-16 13:44:42 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,87 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a dedicated front end developer with skills in web development and content creation. Proficient in technologies such as HTML, CSS, JS, React, After Effects, and intermediate WordPress. Currently a student at Vaigai College of Engineering, passionate about leveraging my knowledge to build impactful solutions.`;
+
+export const ABOUT_TEXT = `I am a passionate full stack developer and content creator, currently pursuing a Bachelor of Engineering in Computer Science at Vaigai College of Engineering. I founded CELITE.IN, a platform for selling After Effects templates, which sharpened my skills in e-commerce, payment gateways, and web development. With experience in freelancing on Fiverr, I excel in technologies like HTML, CSS, JavaScript, React, and After Effects. I'm focused on building innovative web applications, contributing to open-source projects, and solving real-world problems through technology.`;
+
+
+export const EXPERIENCES = [
+  {
+    year: "2023 - Present",
+    role: "Student",
+    company: "Vaigai College of Engineering",
+    description: `Currently pursuing a Bachelor of Engineering in Computer Science while actively developing skills in web technologies and freelancing.`,
+    technologies: ["Fiverr", "Payment Gateways", "Video Editing"],
+  },
+  {
+    year: "2021",
+    role: "Founder & Developer",
+    company: "CELITE.IN",
+    description: `Created a digital product selling website for After Effects templates using WordPress and WooCommerce. Implemented secure payment gateways and optimized the platform for a seamless user experience.`,
+    technologies: ["WordPress", "WooCommerce", "After Effects", "Payment Gateways"],
+  },
+  {
+    year: "2020",
+    role: "MS Office",
+    company: "Independent",
+    description: `I Learned in MS Word, MS Excel, and PowerPoint, gaining essential skills in document creation, data management, and presentation design.`,
+    technologies: ["MS Word", "MS Excel", "PowerPoint"],
+  },
+  {
+    year: "2020",
+    role: "After Effects Video Editor",
+    company: "CELITE.IN",
+    description: `Created logo animations and title animations for CELITE.IN using After Effects. Also worked on remaking title animations from various movies, focusing on motion graphics and dynamic visual effects.`,
+    technologies: ["After Effects", "Logo Animation", "Title Animation", "Motion Graphics"],
+  },
+  {
+    year: "2020 - Present",
+    role: "Content Creator",
+    company: "Chaplin Talks",
+    description: `Started my own YouTube channel, Chaplin Talks, where I discuss and raise awareness on various social issues, focusing on creating impactful and engaging content.`,
+    technologies: ["YouTube", "Content Creation", "Social Issues"],
+  },
+];
+
+
+export const PROJECTS = [
+  {
+    title: "CELITE.IN",
+    image: project1,
+    description:
+      "A digital product selling platform for After Effects templates. Built using WordPress and WooCommerce with features like secure payment gateways, template downloads, and product management.",
+    technologies: ["WordPress", "WooCommerce", "After Effects", "Payment Gateways"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "React", "JavaScript"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS"],
+  },
+  {
+    title: "Arakalagam.com",
+    image: project4,
+    description:
+      "A Wordpress-based platform used to upload political and social-related news for arakalagam.com, featuring posts and commentary on current events.",
+    technologies: ["Wordpress", "HTML", "CSS","Elementor"],
+  },
+];
+
+export const CONTACT = {
+  address: "AK Office, Jawahar Puram, Madurai",
+  phoneNo: "6374401608",
+  email: "talkschaplin@gmail.com",
+  linkedin: "www.linkedin.com/in/chaplinsiva",
+  company: "celite.in",
+};
